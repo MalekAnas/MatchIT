ddd961380caa8eef056c10ca7b8e2adc6796a0fb
Malek Al Harani <malek.horani432@hotmail.com>
2018-10-24 14:59:58 +0200

    Ex 11_7.5 is done \ \\\\\\\\\\\\\\\\\\\\\

--- MatchIT/src/Ex11/Ex11_705.java
+++ MatchIT/src/Ex11/Ex11_705.java
@@ -1,5 +1,44 @@
 package Ex11;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class Ex11_705 {
 
+	private int distinctNums = 0; // feild to define the number of disinct numbers
+
+	public int getDistinctNums() {
+		return distinctNums;
+	}
+
+	public void inputNumbers(int[] x) {
+		Scanner readIn = new Scanner(System.in);
+
+		System.out.println("Enter 10 Numbers: ");
+
+		for (int i = 0; i < x.length; i++) {
+			int number = readIn.nextInt();
+			x[i] = number;
+
+		}
+	}
+
+	public void printDistinct(int x[]) {
+		// First sort the array so that
+		// all occurrences become consecutive
+		Arrays.sort(x); //
+		
+		// Traverse the sorted array
+		for (int i = 0; i < x.length; i++) {
+			distinctNums++;
+			// Move the index ahead while
+			// there are duplicates
+			while (i < x.length - 1 && x[i] == x[i + 1])
+				i++;
+
+			// print last occurrence of
+			// the current element
+			System.out.print(x[i] + " ");
+		}
+	}
 }
--- MatchIT/src/Ex11/Main.java
+++ MatchIT/src/Ex11/Main.java
@@ -1,19 +1,32 @@
 package Ex11;
 
+
 public class Main {
 
 	public static void main(String[] args) {
+//comment to test another Class
+//		double [] myarray = new double [100];
+//		Ex11_704 testArray = new Ex11_704();
+//		testArray.inputScores(myarray);
+//		double sum = testArray.sumValues(myarray);
+//		System.out.println(sum);
+//		double avg = testArray.average(myarray);
+//		System.out.printf("Avg is : %.3f \n" , avg);
+//		testArray.displayScores(myarray);
 
-		double [] myarray = new double [100];
-		Ex11_704 testArray = new Ex11_704();
-
-		testArray.inputScores(myarray);
-		double sum = testArray.sumValues(myarray);
-		System.out.println(sum);
-		double avg = testArray.average(myarray);
-		System.out.printf("Avg is : %.3f \n" , avg);
-		testArray.displayScores(myarray);
-
+		int [] myArray = new int [10];
+		Ex11_705 testMyClass = new Ex11_705();
+		
+		testMyClass.inputNumbers(myArray);
+		
+		
+ 		testMyClass.printDistinct(myArray);
+//	
+ 
+ 		System.out.println("number Of distinct numbers : "+ testMyClass.getDistinctNums());
+//		
+		
+		
 	}
 
 }
