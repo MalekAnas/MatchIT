bc4534097a63e2d21e803555921f44b58f06a288
Pyramid <malek.horani432@hotmail.com>
2018-10-27 14:21:48 +0200

    Ex04 is done

--- MatchIT/src/Ex04/Main.java
+++ MatchIT/src/Ex04/Main.java
@@ -5,6 +5,9 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		System.out.println("Ex04 is done!");
+		//ex04 is almost the same as ex03
+		//with some questions that do not need to write a code to answer them.
 	}
 
 }
