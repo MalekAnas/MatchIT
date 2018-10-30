2e9fa455f0fd948656ec96c04fb9d151f1cf5220
Pyramid <malek.horani432@hotmail.com>
2018-10-30 17:24:16 +0100

    prime Numbers done

--- MatchIT/src/Ex05/Main.java
+++ MatchIT/src/Ex05/Main.java
@@ -3,8 +3,16 @@ package Ex05;
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		
+		
+		
+		
+		
+		
+		
+		
+		
 	}
 
 }
--- MatchIT/src/Ex11/Main.java
+++ MatchIT/src/Ex11/Main.java
@@ -1,9 +1,16 @@
 package Ex11;
 
+import java.util.Scanner;
 
 public class Main {
 
 	public static void main(String[] args) {
+		
+		
+		
+		
+		
+		
 //comment to test another Class
 //		double [] myarray = new double [100];
 //		Ex11_704 testArray = new Ex11_704();
@@ -14,19 +21,48 @@ public class Main {
 //		System.out.printf("Avg is : %.3f \n" , avg);
 //		testArray.displayScores(myarray);
 
-		int [] myArray = new int [10];
-		Ex11_705 testMyClass = new Ex11_705();
-		
-		testMyClass.inputNumbers(myArray);
 		
 		
- 		testMyClass.printDistinct(myArray);
-//	
- 
- 		System.out.println("number Of distinct numbers : "+ testMyClass.getDistinctNums());
-//		
 		
 		
-	}
+//		int [] myArray = new int [10];
+//		Ex11_705 testMyClass = new Ex11_705();
+//		testMyClass.inputNumbers(myArray);
+// 		testMyClass.printDistinct(myArray);
+// 		System.out.println("number Of distinct numbers : "+ testMyClass.getDistinctNums());
+////	
+		
+		
+		
+		Ex11_706 primeFinder = new Ex11_706();
+		
+		
+		primeFinder.checkPrime();
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	
+		
+		
+		
+		
+		
+		
 
+	}
+	
 }
+
+
